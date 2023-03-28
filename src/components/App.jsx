00c9cd73dd0e764fc6bf472edb8922d96d41af4a
ajaxthebug <hey.bhupend@gmail.com>
2023-03-28 21:38:21 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {CssBaseline} from "@mui/material";
+import {Route, Switch } from 'react-router-dom';
+
+
+
+const App = () => (
+    <div>
+        <CssBaseline/>
+        Demo
+    </div>
+)
+
+export default App
